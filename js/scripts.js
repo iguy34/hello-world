19532d03da683c46daa085c2d39c6a516ee15e35
iguy34 <iguy34@hotmail.com>
2024-09-26 02:22:48 -0700

    update HTML and JavaScript for display hello world

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Using console.log
+console.log('Hello World - from JavaScript to log');
+
+// Using document.write
+document.write('Hello World - from JavaScript to HTML');
+
+// Using alert
+// alert('Hello World - from JavaScript to Alert');
